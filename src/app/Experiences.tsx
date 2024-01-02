fbfcfea36cc0f2cead2b792a1c84ec0df89beedd
Osmanys Fuentes-Lombá <ossmanys@gmail.com>
2024-01-01 21:00:41 -0500

    Small changes in styles

--- src/app/Experiences.tsx
+++ src/app/Experiences.tsx
@@ -2,12 +2,12 @@ import experiences from "@/constants/experiences";
 
 const Experience = (item: (typeof experiences)[0]) => {
   return (
-    <li className="p-[17px] rounded-[7px] hover:bg-teal-200">
+    <li className="p-[17px] rounded-[7px] hover:bg-teal-800 cursor-pointer hover:text-teal-50 text-[1.1rem]">
       <p className="">{item.role}</p>
       <p>{item.company}</p>
-      <div className="grid grid-cols-[65%_35%]">
+      <div className="grid grid-cols-[70%_30%]">
         <div className="pr-4">{item.jobDescription}</div>
-        <div className="flex justify-between pl-1">
+        <div className="flex justify-end pl-1">
           <div>
             <span>{item.dateStart}</span> -{" "}
             <span>{!item.current ? item.dateEnd : "Present"}</span>
