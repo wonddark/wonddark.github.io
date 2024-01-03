5c928dd52700c30dd3eb85c3893a249e9355a4f8
Osmanys Fuentes-Lombá <ossmanys@gmail.com>
2024-01-03 12:22:39 -0500

    Small changes in experiences component

--- src/app/Experiences.tsx
+++ src/app/Experiences.tsx
@@ -3,12 +3,12 @@ import experiences from "@constants/experiences";
 const Experience = (item: (typeof experiences)[0]) => {
   return (
     <li className="p-[17px] rounded-[7px] hover:bg-teal-800 cursor-pointer hover:text-teal-50 text-[1.1rem]">
-      <p className="">{item.role}</p>
-      <p>{item.company}</p>
+      <p className="font-semibold text-teal-100">{item.role}</p>
+      <p className="italic my-2">{item.company}</p>
       <div className="grid grid-cols-[70%_30%]">
         <div className="pr-4">{item.jobDescription}</div>
         <div className="flex justify-end pl-1">
-          <div>
+          <div className="italic">
             <span>{item.dateStart}</span> -{" "}
             <span>{!item.current ? item.dateEnd : "Present"}</span>
           </div>
