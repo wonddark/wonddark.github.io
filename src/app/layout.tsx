87f4fe4298d4a688338fed32a1a4231782252c4b
Osmanys Fuentes-Lombá <ossmanys@gmail.com>
2024-01-20 13:50:29 -0500

    Customized some app colors for light/dark themes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { ReactNode } from "react";
-import { Metric, Subtitle, Text, Title } from "@tremor/react";
+import { Bold, Metric, Subtitle, Text, Title } from "@tremor/react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,29 +19,27 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <div className="py-3">
-          <div className="max-w-[1920px] px-3 mx-auto text-center lg:text-right">
-            <Text className="inline-block">
-              <a href="tel:+5355330476">(+53) 553-304-76</a>
-            </Text>
-            <Text className="inline-block ml-2 lg:mr-3.5">
-              <a href="mailto:ossmanys@gmail.com">ossmanys@gmail.com</a>
-            </Text>
-          </div>
-        </div>
-        <div className="grid grid-cols-1 lg:grid-cols-[25%_75%] max-w-[1920px] px-3 pb-3 mx-auto">
-          <div className="lg:pr-2 mb-5 lg:mb-0">
+        <div className="py-3 mb-3 bg-gradient-to-b from-sky-500 to-sky-300 dark:from-slate-950 dark:to-gray-950">
+          <div className="max-w-[1920px] px-3 mx-auto">
+            <div className="text-right">
+              <Bold className="inline-block text-amber-800 dark:text-amber-600">
+                <a href="tel:+5355330476">(+53) 553-304-76</a>
+              </Bold>
+              <Bold className="inline-block text-amber-800 dark:text-amber-600 ml-2 lg:mr-3.5">
+                <a href="mailto:ossmanys@gmail.com">ossmanys@gmail.com</a>
+              </Bold>
+            </div>
             <div className="text-left">
               <Metric>Osmanys Fuentes Lombá</Metric>
               <Title className="mt-1 mb-4">Web Developer</Title>
-              <Subtitle>
+              <Subtitle className="text-amber-800 dark:text-amber-500 dark:opacity-75">
                 Years of experience building B2B web applications
               </Subtitle>
             </div>
           </div>
-          <div className="lg:pl-2">
-            <section>{children}</section>
-          </div>
+        </div>
+        <div className="max-w-[1920px] px-3 pb-3 mx-auto">
+          <section>{children}</section>
         </div>
       </body>
     </html>
