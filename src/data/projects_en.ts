acb3fead7656cfebff64bb126f8ccae750ee6e71
Osmanys Fuentes-Lombá <ossmanys@gmail.com>
2024-05-28 22:52:39 -0400

    A list of current projects

---
+++ src/data/projects_en.ts
@@ -0,0 +1,28 @@
+const projects: {name:string; thumbnail:string;url:string;description:string;skills:string[];status:string}[] = [
+  {
+    name: 'Events Booking',
+    thumbnail: '/domenico-loia-hGV2TfOh0ns-unsplash.webp',
+    url: '',
+    description: 'A system for finding, posting and sharing events as well as buying tickets',
+    skills:['react', 'typescript', 'remix', 'postgresql'],
+    status:'Active'
+  },
+  {
+    name: 'DataShop',
+    thumbnail: '/domenico-loia-hGV2TfOh0ns-unsplash.webp',
+    url: '',
+    description: 'A system for finding, posting and sharing events as well as buying tickets',
+    skills:['react', 'typescript', 'remix', 'postgresql'],
+    status:'Active'
+  },
+  {
+    name: 'Kingdom Library',
+    thumbnail: '/domenico-loia-hGV2TfOh0ns-unsplash.webp',
+    url: '',
+    description: 'A system for finding, posting and sharing events as well as buying tickets',
+    skills:['react', 'typescript', 'remix', 'postgresql'],
+    status:'Active'
+  },
+]
+
+export default projects
