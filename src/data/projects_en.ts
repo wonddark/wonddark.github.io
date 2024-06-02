c9bbba0f6b732ce98f4c6ea37bff5a41aab3bede
Osmanys Fuentes-Lombá <ossmanys@gmail.com>
2024-06-01 23:05:07 -0400

    Added data to some projects

--- src/data/projects_en.ts
+++ src/data/projects_en.ts
@@ -1,27 +1,39 @@
-const projects: {name:string; thumbnail:string;url:string;description:string;skills:string[];status:string}[] = [
+const projects: {name:string; thumbnail:string;url:string;description:string;skills:string[];status:string;projectPage:string;}[] = [
   {
     name: 'Events Booking',
     thumbnail: '/images/projects/eventsbooking_desktop.webp',
-    url: '',
+    url: 'https://eventsbooking.vercel.app',
     description: 'A system for finding, posting and sharing events as well as buying tickets',
     skills:['react', 'remix', 'tailwindcss', 'typescript', 'postgresql'],
-    status:'Active'
+    status:'Active',
+    projectPage:'/projects/events-booking'
   },
   {
     name: 'DataShop',
     thumbnail: '/domenico-loia-hGV2TfOh0ns-unsplash.webp',
-    url: '',
+    url: 'https://any-data-generator.vercel.app',
     description: 'A system for finding, posting and sharing events as well as buying tickets',
     skills:['react', 'typescript', 'remix', 'postgresql'],
-    status:'Active'
+    status:'Active',
+    projectPage:'/projects/data-shop'
+  },
+  {
+    name: 'Must Read Them',
+    thumbnail: '/images/projects/must-read-them_desktop.webp',
+    url: 'https://must-read-them.vercel.app',
+    description: 'A system for generating curated books lists according to user defined goals. Each list is tailored to speed up the skill mastering process.',
+    skills:['react', 'typescript', 'remix', 'postgresql'],
+    status:'Active',
+    projectPage:'/projects/must-read-them'
   },
   {
     name: 'Kingdom Library',
     thumbnail: '/domenico-loia-hGV2TfOh0ns-unsplash.webp',
-    url: '',
+    url: 'https://kingdomlibrary.vercel.app',
     description: 'A system for finding, posting and sharing events as well as buying tickets',
     skills:['react', 'typescript', 'remix', 'postgresql'],
-    status:'Active'
+    status:'Active',
+    projectPage:'/projects/kingdom-library'
   },
 ]
 
