b8de70143626020ce9da4a0081b50216c3dc05dc
Osmanys Fuentes-Lombá <ossmanys@gmail.com>
2024-06-04 20:30:17 -0400

    Updated the header navbar for other languages than english

--- src/components/es/HeaderMenu.tsx
+++ src/components/es/HeaderMenu.tsx
@@ -12,33 +12,34 @@ function HeaderMenu() {
       window.onhashchange = null;
     };
   }, []);
+
   return (
     <div
-      className="h-fit fixed top-0 left-0 right-0 z-50 bg-white dark:bg-[#131516] top-nav hidden md:block">
+      className="h-fit fixed top-0 left-0 right-0 z-50 top-nav hidden md:block">
       <nav className="w-fit mx-auto pt-14 pb-9">
         <ul
-          className="flex gap-8 font-medium leading-[1.25] tracking-[0.08em] underline-offset-4">
+          className="flex gap-8">
           <li>
             <a href="#home"
-               className={`${currentSection === "" || currentSection === "#home" ? "border-b border-[#009394] text-[#009394] hover:brightness-125" : "hover:underline hover:text-[#009394]"} px-1.5`}>Inicio</a>
+               className={`${currentSection === "" || currentSection === "#home" ? "border-b border-primary text-primary hover:brightness-125" : "text-default hover:underline hover:text-primary"} px-1.5 pb-1`}>Inicio</a>
           </li>
           <li>
             <a href="#about"
-               className={`${currentSection === "#about" ? "border-b border-[#009394] text-[#009394] hover:brightness-125" : "hover:border-b hover:border-[#009394] hover:text-[#009394]"} px-1.5`}>Info</a>
+               className={`${currentSection === "#about" ? "border-b border-primary text-primary hover:brightness-125" : "text-default hover:border-b hover:border-primary hover:text-primary"} px-1.5 pb-1`}>Bio</a>
           </li>
           <li>
             <a href="#experience"
-               className={`${currentSection === "#experience" ? "border-b border-[#009394] text-[#009394] hover:brightness-125" : "hover:border-b hover:border-[#009394] hover:text-[#009394]"} px-1.5`}>Experiencia</a>
+               className={`${currentSection === "#experience" ? "border-b border-primary text-primary hover:brightness-125" : "text-default hover:border-b hover:border-primary hover:text-primary"} px-1.5 pb-1`}>Experiencia</a>
           </li>
           <li>
             <a href="#projects"
-               className={`${currentSection === "#projects" ? "border-b border-[#009394] text-[#009394] hover:brightness-125" : "hover:border-b hover:border-[#009394] hover:text-[#009394]"} px-1.5`}>Proyectos</a>
+               className={`${currentSection === "#projects" ? "border-b border-primary text-primary hover:brightness-125" : "text-default hover:border-b hover:border-primary hover:text-primary"} px-1.5 pb-1`}>Proyectos</a>
           </li>
           <li>
             <a href="#contact"
-               className={`${currentSection === "#contact" ? "border-b border-[#009394] text-[#009394] hover:brightness-125" : "hover:border-b hover:border-[#009394] hover:text-[#009394]"} px-1.5`}>Contacto</a>
+               className={`${currentSection === "#contact" ? "border-b border-primary text-primary hover:brightness-125" : "text-default hover:border-b hover:border-primary hover:text-primary"} px-1.5 pb-1`}>Contacto</a>
           </li>
-          <li>
+          <li className="relative">
             <LanguageSelector />
           </li>
         </ul>
