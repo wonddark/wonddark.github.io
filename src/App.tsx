8694fc036ed225f12e983ceb4c272bd0a39f6ae8
Osmanys Fuentes L <ossmanys@gmail.com>
2025-03-08 13:45:22 +0400

    First commit after reset

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <main className="grid min-h-screen place-content-center place-items-center">
+      <p className="text-center text-3xl">Under construction!</p>
+    </main>
+  );
+}
+
+export default App;
