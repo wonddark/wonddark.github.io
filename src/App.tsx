fe0e38e8698b0d59bcdf7dc62ba15a9a0d44f58b
Osmanys Fuentes L <ossmanys@gmail.com>
2025-03-08 14:03:40 +0400

    Using a bigger font-size

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 function App() {
   return (
     <main className="grid min-h-screen place-content-center place-items-center">
-      <p className="text-center text-3xl">Under construction!</p>
+      <p className="text-center text-4xl">Under construction!</p>
     </main>
   );
 }
